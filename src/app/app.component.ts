12dbe014d0ee12dddc75830ba0e9524ea236b495
Lourenco Werneck <lourencovw@gmail.com>
2022-05-24 14:06:33 -0300

    feat(search): save search string to store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IAppState } from './models/app-state.model';
+import { addSearch } from './store/actions/search.action';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aplazo';
+
+  constructor(private store: Store<IAppState>) {
+  }
+
+  search(event: any) {
+    this.store.dispatch(addSearch({ payload: event.target.value }))
+  }
 }
