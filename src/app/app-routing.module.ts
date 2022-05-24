c50fb59be9df33269e5cd80aa7a388a4df24df24
Lourenco Werneck <lourencovw@gmail.com>
2022-05-24 14:05:24 -0300

    feat(routes): add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+
+interface IListType {
+  listType: string;
+}
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'characters',
+    pathMatch: 'full',
+  },
+  {
+    path: 'characters',
+    data: <IListType>{ listType: 'character' },
+    component: ListComponent
+  },
+  {
+    path: 'locations',
+    data: <IListType>{ listType: 'location' },
+    component: ListComponent
+  },
+  {
+    path: 'episodes',
+    data: <IListType>{ listType: 'episode' },
+    component: ListComponent
+  },
+  {
+    path: ':type/details/:id',
+    loadChildren: () => import('./components/detail/detail.module').then(m => m.DetailModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./components/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
